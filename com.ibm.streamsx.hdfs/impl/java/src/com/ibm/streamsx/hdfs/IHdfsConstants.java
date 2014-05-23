cba712c8e8bd5abd48ef35c23689dffc4f1a3f49
chanskw <chanskw@ca.ibm.com>
2014-05-22 21:18:00 -0400

    Fixed copyright, fixed build.xml to include properties file

--- com.ibm.streamsx.hdfs/impl/java/src/com/ibm/streamsx/hdfs/IHdfsConstants.java
+++ com.ibm.streamsx.hdfs/impl/java/src/com/ibm/streamsx/hdfs/IHdfsConstants.java
@@ -1,17 +1,11 @@
-/* begin_generated_IBM_copyright_prolog                             */
-/*                                                                  */
-/* This is an automatically generated copyright prolog.             */
-/* After initializing,  DO NOT MODIFY OR MOVE                       */
-/* **************************************************************** */
-/* IBM Confidential                                                 */
-/* OCO Source Materials                                             */
-/* 5724-Y95                                                         */
-/* (C) Copyright IBM Corp.  2013, 2014                              */
-/* The source code for this program is not published or otherwise   */
-/* divested of its trade secrets, irrespective of what has          */
-/* been deposited with the U.S. Copyright Office.                   */
-/*                                                                  */
-/* end_generated_IBM_copyright_prolog                               */
+/*******************************************************************************
+ * Licensed Materials - Property of IBM
+ * Copyright IBM Corp. 2014
+ * US Government Users Restricted Rights - Use, duplication or
+ * disclosure restricted by GSA ADP Schedule Contract with
+ * IBM Corp.
+ *******************************************************************************/
+
 package com.ibm.streamsx.hdfs;
 
 public interface IHdfsConstants {
--- com.ibm.streamsx.hdfs/impl/java/src/com/ibm/streamsx/hdfs/client/auth/HDFSAuthenticationHelper.java
+++ com.ibm.streamsx.hdfs/impl/java/src/com/ibm/streamsx/hdfs/client/auth/HDFSAuthenticationHelper.java
@@ -1,17 +1,11 @@
-/* begin_generated_IBM_copyright_prolog                             */
-/*                                                                  */
-/* This is an automatically generated copyright prolog.             */
-/* After initializing,  DO NOT MODIFY OR MOVE                       */
-/* **************************************************************** */
-/* IBM Confidential                                                 */
-/* OCO Source Materials                                             */
-/* 5724-Y95                                                         */
-/* (C) Copyright IBM Corp.  2014, 2014                              */
-/* The source code for this program is not published or otherwise   */
-/* divested of its trade secrets, irrespective of what has          */
-/* been deposited with the U.S. Copyright Office.                   */
-/*                                                                  */
-/* end_generated_IBM_copyright_prolog                               */
+/*******************************************************************************
+ * Licensed Materials - Property of IBM
+ * Copyright IBM Corp. 2014
+ * US Government Users Restricted Rights - Use, duplication or
+ * disclosure restricted by GSA ADP Schedule Contract with
+ * IBM Corp.
+ *******************************************************************************/
+
 package com.ibm.streamsx.hdfs.client.auth;
 
 import java.io.IOException;
@@ -38,16 +32,6 @@ import java.util.Map;
 import java.util.logging.Logger;
 
 public class HDFSAuthenticationHelper extends BaseAuthenticationHelper {
-	/* begin_generated_IBM_copyright_code                               */
-	public static final String IBM_COPYRIGHT =
-		" Licensed Materials-Property of IBM                              " + //$NON-NLS-1$ 
-		" 5724-Y95                                                        " + //$NON-NLS-1$ 
-		" (C) Copyright IBM Corp.  2014, 2014    All Rights Reserved.     " + //$NON-NLS-1$ 
-		" US Government Users Restricted Rights - Use, duplication or     " + //$NON-NLS-1$ 
-		" disclosure restricted by GSA ADP Schedule Contract with         " + //$NON-NLS-1$ 
-		" IBM Corp.                                                       " + //$NON-NLS-1$ 
-		"                                                                 " ; //$NON-NLS-1$ 
-	/* end_generated_IBM_copyright_code                                 */
 
 	private static Logger TRACE = Logger
 			.getLogger("AuthenticationHelper.class");
