a6e85090051180486f7f55c9a8cedf91e42b08bf
natashadsilva <ndsilva@ca.ibm.com>
2015-11-13 19:37:39 -0500

    updated sample and changed client to load keystore only once

--- com.ibm.streamsx.hdfs/impl/java/src/com/ibm/streamsx/hdfs/client/webhdfs/URLUtils.java
+++ com.ibm.streamsx.hdfs/impl/java/src/com/ibm/streamsx/hdfs/client/webhdfs/URLUtils.java
@@ -1,20 +1,3 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.ibm.streamsx.hdfs.client.webhdfs;
 
@@ -22,10 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.security.KeyManagementException;
 import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
@@ -35,29 +15,12 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
-import org.apache.hadoop.classification.InterfaceAudience;
-import org.apache.hadoop.classification.InterfaceStability;
-import org.apache.hadoop.conf.Configuration;
-
-import com.ibm.streamsx.hdfs.IHdfsConstants;
-
-/**
- * Utilities for handling URLs
- */
-@InterfaceAudience.LimitedPrivate({"HDFS"})
-@InterfaceStability.Unstable
 public class URLUtils {
 	/**
 	 * Timeout for socket connects and reads
 	 */
-	public static int SOCKET_TIMEOUT = 1*60*1000; // 1 minute
+	public static int TIMEOUT = 1*60*1000; // 1 minute
 
-	/**
-	 * Opens a url with read and connect timeouts
-	 * @param url to open
-	 * @return URLConnection
-	 * @throws IOException
-	 */
 	public static URLConnection openConnection(URL url) throws IOException {
 		URLConnection connection = url.openConnection();
 		setTimeouts(connection);
@@ -70,8 +33,8 @@ public class URLUtils {
 	 * @param connection URLConnection to set
 	 */
 	static void setTimeouts(URLConnection connection) {
-		connection.setConnectTimeout(SOCKET_TIMEOUT);
-		connection.setReadTimeout(SOCKET_TIMEOUT);
+		connection.setConnectTimeout(TIMEOUT);
+		connection.setReadTimeout(TIMEOUT);
 	}
 	private static TrustManager[] getAcceptAllTrustManager() {
 		TrustManager[] trustAllCerts = new TrustManager[] {
@@ -95,6 +58,11 @@ public class URLUtils {
 		};
 		return trustAllCerts;
 	}
+	/**
+	 * Creates a socket factory that is configured to use the keystore indicated by the
+	 * @param keyStore and
+	 * @param keyStorePassword parameters.
+	 * */
 	public static SSLSocketFactory getSocketFactory(String keyStore,
 			String keyStorePassword) throws Exception {
 
